97f9fd817da22cb5da5f9a8eb410cd82ead5f4e3
Sbonelo Gumede <gmdsbo006@myuct.ac.za>
2025-07-22 15:29:11 +0200

    created train and test splits

--- airquality/code/EDA.R
+++ airquality/code/EDA.R
@@ -2,47 +2,77 @@
 rm(list = ls())
 
 # Attach the required packages.
-required_packages <- c("corrplot", "dplyr", "readxl", "tidyverse")
+required_packages <- c("corrplot", "dplyr", "ggplot2", "readxl")
 for (package in required_packages) {
    if (!requireNamespace(package = package, quietly = TRUE)) {
       install.packages(pkgs = package, quiet = TRUE)
    }
-   library(package = package, character.only = TRUE)
+   require(package = package, character.only = TRUE)
 }
 
 # Import the data.
-file_names <- c(
-   "2019_NO2_CCT.xls",
-   "2019_PM10_CCT.xls",
-   "2019_SO2_CCT.xls",
-   "Wind_direction_and_speed_2019.xlsx",
-   "Wind_direction_and_speed_2019.xlsx"
-)
-cell_ranges <- c("E6:E8742", "E6:E8742", "H6:H8742", "N6:N8742", "O6:O8742")
-dat <- read_excel(
-   path = paste("..", "data", file_names[1], sep = "/"),
-   range = "A6:A8742",
-   col_names = FALSE
-) %>%
-   pull(1) %>%
-   as.POSIXct(format = "%d/%m/%Y %H:%M")
+years <- c(2019, 2020)
+data_storage <- list(NA, NA)
 
-for (i in 1:5) {
-   dat <- cbind(dat, read_excel(
-      path = paste("..", "data", file_names[i], sep = "/"),
-      range = cell_ranges[i],
-      col_names = FALSE,
-      col_types = "numeric",
-      na = c("Down", "InVld", "NoData", "Zero", "<Samp")
-   ))
+for(i in 1:2){
+   file_names <- c(
+      paste(years[i], "NO2_CCT.xls", sep="_"),
+      paste(years[i], "PM10_CCT.xls", sep="_"),
+      paste(years[i], "SO2_CCT.xls", sep="_"),
+      paste("Wind_direction_and_speed_", years[i], ".xlsx", sep=""),
+      paste("Wind_direction_and_speed_", years[i], ".xlsx", sep="")
+   )
+   
+   cell_ranges <- matrix(
+      data = c(c("E6:E8742", "E6:E8742", "H6:H8742", "N6:N8742", "O6:O8742"),
+               c("G6:G8742", "G6:G8742", "I6:I8742", "N6:N8742", "O6:O8742")), 
+      nrow = 2, ncol = 5, byrow = TRUE)
+   
+   temp <- read_excel(
+      path = paste("..", "data", file_names[1], sep = "/"),
+      range = "A6:A8742",
+      col_names = FALSE) %>%
+      pull(1) %>%
+      as.POSIXct(format = "%d/%m/%Y %H:%M")
+   
+   for (j in 1:5) {
+      temp <- cbind(temp, read_excel(
+         path = paste("..", "data", file_names[j], sep = "/"),
+         range = cell_ranges[i, j],
+         col_names = FALSE,
+         col_types = "numeric",
+         na = c("Down", "InVld", "NoData", "Zero", "<Samp")
+      ))
+   }
+   colnames(temp) <- c("DateTime", "NO2", "PM10", "SO2", "Direction", "Speed")
+   data_storage[[i]] <- temp
 }
-colnames(dat) <- c("DateTime", "NO2", "PM10", "SO2", "Direction", "Speed")
-head(dat)
 
-cor_matrix <- dat %>%
+# Create train and test splits.
+train <- data_storage[[1]]
+test <- data_storage[[2]]
+
+# Calculate the pairwise correlation between variables.
+cor_matrix <- train %>%
    select(-DateTime) %>%
    cor(use = "na.or.complete", method = "pearson")
 corrplot(corr = cor_matrix, method = "color", type = "lower")
 
-plot(x = dat$NO2, main = "", xlab = "Date", ylab = "NO2", col = "steelblue", pch = 18)
-hist(x = dat$NO2, main = "", xlab = "NO2", col = "steelblue")
+# Scatter plot of the response variable.
+ggplot(data = train, mapping = aes(x = DateTime, y = NO2))+
+   geom_point(color = "steelblue", size = 2, pch = 18)+
+   theme_minimal()
+
+# Histogram of the response variable.
+ggplot(data = train, mapping = aes(x = NO2))+
+   geom_histogram(mapping = aes(y = ..density..), 
+                  color = "lightskyblue", fill = "steelblue")+
+   labs(x=expression(NO[2]))+
+   theme_minimal()
+
+# Histogram of the log of the response variable.
+ggplot(data = train, mapping = aes(x = log(NO2)))+
+   geom_histogram(mapping = aes(y = ..density..), 
+                  color = "lightskyblue", fill = "steelblue")+
+   labs(x=expression(log(NO[2])))+
+   theme_minimal()
