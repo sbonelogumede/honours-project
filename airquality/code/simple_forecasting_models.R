f1f9a663cf2cc2c38b33f9c8f597b6012c0bf361
Sbonelo Gumede <gmdsbo006@myuct.ac.za>
2025-08-14 13:16:22 +0200

    added ts plots

--- airquality/code/simple_forecasting_models.R
+++ airquality/code/simple_forecasting_models.R
@@ -1,5 +1,6 @@
 require(package="forecast")
 require(package="fpp3")
+require(package="gridExtra")
 require(package="zoo")
 
 load(file="../objects/data_storage.RData")
@@ -11,49 +12,75 @@ x <- 1 : nrow(train)
 train_no2 <- train$NO2
 
 # Add some small epsilon where NO2 = 0.
-train_no2[train_no2 == 0] <- 0.001
+train_no2[train_no2 == 0] <- 1
 # Running linear interpolation for NA's.
 train_no2 <- na.approx(object=train_no2)
 
-# Display the data.
-par(mfrow=c(1, 3))
-plot(x=x, y=train_no2, main="", xlab="Time", ylab=expression(NO[2]), 
-	  cex=0.5, col="lightskyblue", pch=18)
-ggAcf(x=train_no2) + labs(title="") + theme_minimal()
-ggPacf(x=train_no2) + labs(title="") + theme_minimal()
+ts_plots <- function(x, label="y = f(x)", xlab="x", ylab="y"){
+	p1 <- autoplot(object=x) +
+		autolayer(ma(x=x, order=718), series="MA718", linewidth=1.5) +
+		ggtitle(label=label) +
+		xlab(label="Time") +
+		ylab(label=ylab) +
+		guides(colour=guide_legend(title="Model")) +
+		theme_minimal()
+	p2 <- ggAcf(x=x) +
+		labs(title=label) + 
+		theme_minimal()
+	p3 <- ggPacf(x=x) + 
+		labs(title=label) + 
+		theme_minimal()
+	arrangeGrob(p1, p2, p3, nrow=1)
+}
+
+train_no2_ts <- ts(data=train_no2, frequency=24)
+p1 <- ts_plots(x=train_no2_ts, 
+					label=expression("Series:" ~ NO[2]), 
+					ylab=expression(NO[2]))
+ggsave(filename="../images/no2_ts_2019.png", plot=p1, width=24, height=6, 
+		 units="in", dpi=600)
 
 # Check if there is a need to stabilize variance.
-BoxCox.lambda(x=train_no2, method="guerrero")
+BoxCox.lambda(x=train_no2_ts, method="guerrero")
 
 # Stabilize variance.
-log_train_no2 <- log(train_no2)
-
-# Display the variance stabilized data.
-plot(x=x, y=log_train_no2, main="", xlab="Time", ylab=expression(ln(NO[2])), 
-	  cex=0.5, col="lightskyblue", pch=18, ylim=c(0, 5))
-ggAcf(x=log_train_no2) + labs(title="") + theme_minimal()
-ggPacf(x=train_no2) + labs(title="") + theme_minimal()
+train_log_no2_ts <- log(x=train_no2_ts)
+p2 <- ts_plots(x=train_log_no2_ts, 
+					label=expression("Series:" ~ ln(NO[2])), 
+					ylab=expression(ln(NO[2])))
+ggsave(filename="../images/log_no2_ts_2019.png", plot=p2, width=24, height=6, 
+		 units="in", dpi=600)
 
 # Check if there is a need for removing seasonality.
-nsdiffs(x=log_train_no2)
+nsdiffs(x=train_log_no2_ts)
+
+# Check if there is a need for removing trend.
+ndiffs(x=train_log_no2_ts)
+
+train_diff_log_no2_ts <- diff(x=train_log_no2_ts)
+p3 <- ts_plots(x=train_diff_log_no2_ts, 
+					label=expression("Series:" ~ ln(NO[2])), 
+					ylab=expression(ln(NO[2])))
+ggsave(filename="../images/diff_log_no2_ts_2019.png", plot=p3, width=24, height=6, 
+		 units="in", dpi=600)
 
 # Average model fit.
-average_model <- meanf(y=train_no2, h=3)
+average_model <- meanf(y=train_no2_ts, h=3)
 average_model
 
 # Naive model fit.
-naive_model <- naive(y=train_no2, h=3)
+naive_model <- naive(y=train_no2_ts, h=3)
 naive_model
 
 # Seasonal naive model fit.
-snaive_model <- snaive(y=train_no2, h=3)
+snaive_model <- snaive(y=train_no2_ts, h=3)
 snaive_model
 
 # Drift model fit.
-drift_model <- rwf(y=train_no2, h=3, drift=T)
+drift_model <- rwf(y=train_no2_ts, h=3, drift=T)
 drift_model
 
 # ARIMA model fit.
-arima_model <- arima(x=train_no2, order=c(0, 1, 1), seasonal=list(order=c(0, 1, 1)))
+arima_model <- arima(x=train_no2_ts, order=c(0, 1, 1), seasonal=list(order=c(0, 1, 1)))
 arima_preds <- predict(object=arima_model, h=3)
 arima_preds
