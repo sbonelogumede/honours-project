face0b5d32cde2f809d32f4e7d66c29f145b8253
Sbonelo Gumede <gmdsbo006@myuct.ac.za>
2025-09-08 22:47:46 +0200

    Likelihood approach attempt and time series write-up.

--- airquality/code/GP_0_WN.R
+++ airquality/code/GP_0_WN.R
@@ -1,18 +1,15 @@
 require(package = "rstan")
 require(package = "parallel")
 
-h_vector <- c(24, 168, 744)
-R <- matrix(data=NA, nrow=3, ncol=2, dimnames=list(h_vector, c("RMSE", "MAE")))
+h_vec <- c(24, 168, 744)
+R <- matrix(data = NA, nrow = 3, ncol = 2, dimnames = list(h_vec, c("RMSE", "MAE")))
 
 for(i in 1:3){
-	load(file = paste0("../object/", h_vector[i], "h_parameters.RData"))
-	parameters <- list(n1 = n1, p = p, X1 = X1, y1 = log(y1), n2 = n2, X2 = X2)
-	coresNumber <- detectCores() - 2
+	load(file = paste0("../object/", h_vec[i], "h_parameters.RData"))
+	par <- list(n1 = n1, p = p, X1 = X1, y1 = log(y1), n2 = n2, X2 = X2)
+	cn <- detectCores() - 2
 	
-	stan_fit <- stan(file = "GP_0_WN.stan", 
-						  cores = coresNumber,
-						  data = parameters, 
-						  verbose = FALSE)
+	stan_fit <- stan(file = "GP_0_WN.stan", cores = cn, data = par, verbose = F)
 	stan_summary <- (summary(stan_fit))$summary
 	
 	y2_hat <- stan_summary[grep("^y2", rownames(stan_summary)), "mean"] |> 
